cd5222d99affdc5361c74890aaa06913395456fd
Myeonghwan Yu <gmt1361@gmail.com>
2024-02-27 14:47:04 +0900

    9th commit

--- src/routes/owner.router.js
+++ src/routes/owner.router.js
@@ -1,116 +1,21 @@
 import express from 'express';
-import { prisma } from '../models/index.js';
-
+import { prisma } from '../utils/prisma/index.js';
+import {OwnerService} from '../services/owner.service.js';
+import {OwnerRepository} from '../repositories/owner.repository.js';
+import { OwnerController } from '../controllers/owner.controller.js';
 import {AuthController} from '../middlewares/auth/auth.middleware.controller.js';
 import {AuthService} from '../middlewares/auth/auth.middleware.service.js';
 
-const authService = new AuthService(usersRepository);
+const router = express.Router();
+const ownerRepository = new OwnerRepository(prisma);
+const ownerService = new OwnerService(ownerRepository);
+const ownerController = new OwnerController(ownerService);
+
+const authService = new AuthService(ownerRepository);
 const authController = new AuthController(authService);
 
-// id와 onwerid 구분해야하나?????????
+router.post('/:ownerId/restaurant', authController.authMiddleWare, ownerController.createRestaurant);
 
-const router = express.Router();
+router.patch('/:ownerId/restaurant/:restaurantId', authController.authMiddleWare, ownerController.updateRestaurant);
 
-router.post('/:ownerId/restaurant', authController.authMiddleWare, async (req, res, next) => {
-
-    try{
-        const { name, callNumber, kind, restaurantInfo } = req.body;
-        const {ownerId} = req.user;
-        const isowner = await prisma.users.findFirst({
-            where: {
-                id: +ownerId,
-            }, select: {
-                type: true,
-            }
-        })
-
-        if(isowner === guest) {
-            return res.status(400).json({message: "사장님만 가능한 작업입니다."});
-        }
-
-        const restaurant = await prisma.restaurant.create({
-            data: {
-                userId: +ownerId,
-                name: name,
-                callNumber: callNumber,
-                kind: kind,
-                restaurantInfo: restaurantInfo
-            }
-        });
-
-        return res.status(200).json({data: restaurant})
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({message: '에러가 발생했습니다.'});
-    }
-})
-
-router.patch('/:ownerId/restaurant/:restaurantId', authController.authMiddleWare, async (req, res, next) => {
-    try {
-        const { restaurantId } = req.params;
-        const { name, callNumber, kind, restaurantInfo } = req.body;
-        const { ownerId } = req.user;
-
-        const existingRestaurant = await prisma.restaurant.findFirst({
-            where: {
-                restaurantId: +restaurantId,
-            }
-        });
-
-        if(!existingRestaurant){
-            return res.status(404).json({message: "레스토랑 조회에 실패했습니다."});
-        }
-        if(existingRestaurant.id !== +ownerId) {
-            return res.status(403).json({message: "본인의 레스토랑이 아닙니다."});
-        }
-
-        const updatedRestaurant = await prisma.restaurant.update({
-            where: {
-                restaurantId: existingRestaurant.restaurantId,
-            },
-            data: {
-                name: name || existingRestaurant.name,
-                callNumber: callNumber || existingRestaurant.callNumber,
-                kind: kind || existingRestaurant.kind,
-                restaurantInfo: restaurantInfo || existingRestaurant.restaurantInfo,
-            },
-        });
-
-        return res.status(200).json({data: updatedRestaurant});
-
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({message: "에러가 발생했습니다."});
-    }
-})
-
-router.delete('/:ownerId/restaurant/:restaurantId', authController.authMiddleWare, async (req, res, next) => {
-    try{
-        const {restaurantId} = req.params;
-        const existingRestaurant = await prisma.restaurant.findFirst({
-            where: {
-                restaurantId: +restaurantId,
-            }
-        });
-        
-        if(!existingRestaurant) {
-            return res.status(404).json({message: "레스토랑 조회에 실패했습니다."});
-        }
-
-        const {ownerId} = req.user;
-        if(existingRestaurant.id !== +ownerId) {
-            return res.status(403).json({message: "본인의 레스토랑이 아닙니다."})
-        }
-
-        await prisma.restaurant.delete({
-            where: {
-                restaurantId: +restaurantId,
-            },
-        });
-
-        return res.status(200).json({message: "레스토랑 정보가 성공적으로 삭제되었습니다."})
-    } catch(err) {
-        console.log(err);
-        return res.status(500).json({message: "에러가 발생했습니다."});
-    }
-})
+router.delete('/:ownerId/restaurant/:restaurantId', authController.authMiddleWare, ownerController.deleteRestaurant);
